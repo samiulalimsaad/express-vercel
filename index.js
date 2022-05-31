fe553b78d8ec4ecc3bfac09b254c90b87081ac9c
samiulalimsaad <saadnbiuraj@gmail.com>
2022-05-31 10:06:38 +0600

    4

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ app.get("/", (req, res) => {
     res.json({ message: "hello" });
 });
 
+app.get("/about", (req, res) => {
+    res.json({ message: "about page working" });
+});
+
 app.listen(PORT, () => console.log("server is running"));
